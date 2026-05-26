9a8decddafa4b324fbc669cc74009c7acfe368f5
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2026-05-26 02:27:14 -0300

    Neovimにさらにプラグインを追加しました

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,17 +1,19 @@
 -- Load all plugins
-require("plugins.treesitter")
-require("plugins.mason")
-require("plugins.tokyonight")
-require("plugins.mini")
-require("plugins.neo-tree")
-require("plugins.blink")
-require("plugins.rustacean")
-require("plugins.bufferline")
-require("plugins.gitsigns")
-require("plugins.telescope")
-require("plugins.trouble")
-require("plugins.which-key")
-require("plugins.lua-line")
-require("plugins.render-markdown")
-require("plugins.toggleterm")
-require("plugins.fidget")
+require("plugins.treesitter") -- Syntax Highlight
+require("plugins.mason") -- LSP plugin
+require("plugins.tokyonight") -- Theme
+require("plugins.mini") -- Misc. plugins
+require("plugins.neo-tree") -- File/Buffer tree
+require("plugins.blink") -- auto completion
+require("plugins.rustacean") -- Rust Falopa
+require("plugins.bufferline") -- Buffer Tabs
+require("plugins.gitsigns") -- Git
+require("plugins.telescope") -- File selector
+require("plugins.trouble") -- Diagnostics
+require("plugins.which-key") -- Show keymaps
+require("plugins.lua-line") -- Better Status Line
+require("plugins.render-markdown") -- Better Markdown
+require("plugins.toggleterm") -- Show Terminal
+require("plugins.fidget") -- Better nvim notifications
+require("plugins.todo-comment") -- Better comments
+require("plugins.colorizer") -- Show HEX-colors
